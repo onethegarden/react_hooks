b5e0990252dce9f1c439e417fc4f3c4c4e3225ae
onethegarden <onethegarden@gmail.com>
2020-03-13 07:54:41 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-const content =[
-  {
-    tab:"Section 1",
-    content:"I'm the content"
-  },
-  {
-    tab:"Section 2",
-    content:"I'm the content"
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  
- const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if(!allTabs || !Array.isArray(allTabs)){
-    return;
-  }else{
-    return{
-      currentItem: allTabs[currentIndex],
-      changeItem:setCurrentIndex
-    };
-  }
-};
 const App = () => {
-  const {currentItem, changeItem} = useTabs(0, content);
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const[aNumber, setAnumber] = useState(0);
   return (
     <div>
-     <h1> Hello</h1>
-     {content.map((section, index) =>(
-       <button onClick={()=> changeItem(index)}>{section.tab}</button>
-     ))}
-     <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button onClick={()=>setNumber(number + 1)}>{number}</button>
+      <button onClick={()=>setAnumber(aNumber - 1)}>{aNumber}</button>
     </div>
   );
 }
---
+++ src/nooks/useTabs.js
@@ -0,0 +1,13 @@
+
+export const useTabs = (initialTab, allTabs) => {
+  
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+     if(!allTabs || !Array.isArray(allTabs)){
+       return;
+     }else{
+       return{
+         currentItem: allTabs[currentIndex],
+         changeItem:setCurrentIndex
+       };
+     }
+   };
