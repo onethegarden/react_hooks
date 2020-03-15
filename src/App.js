e313b7b658b5322df0368d9038a33e71f3ebdd27
onethegarden <onethegarden@gmail.com>
2020-03-16 07:43:23 +0900

    setTitle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const useTitle = (initialTitle)=>{
+  const [title, setTitle]= useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  }
+  useEffect(updateTitle, [title]);
+  return setTitle;
+}
+
 const App = () => {
-  const sayHello = () => console.log("hello");
-  const [number, setNumber] = useState(0);
-  const[aNumber, setAnumber] = useState(0);
-  useEffect(sayHello,[]);//fisrt : function, second : depend
-  //useEffect : componentDidMount, ComponenetWillUnMoount, ComponentDidUpdate
+  const titleUpdater = useTitle("Loading...");
   return (
     <div>
       <div>Hi</div>
-      <button onClick={()=>setNumber(number + 1)}>{number}</button>
-      <button onClick={()=>setAnumber(aNumber - 1)}>{aNumber}</button>
     </div>
   );
 }
