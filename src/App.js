4fa9eddd03a8ce1db1d469bd32fe96dd460ee5fa
onethegarden <onethegarden@gmail.com>
2020-03-23 21:08:40 +0900

    useScroll

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, {useState, useRef, useEffect} from 'react';
 import './App.css';
 
-const useNetwork = onChange => {
-  const [status, setStatus] = useState(navigator.onLine);
-  const handleChange = () => {
-    if(typeof onChange === "function"){
-      onChange(navigator.onLine);
-    };
-    setStatus(navigator.onLine)
-  }
-  useEffect (() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
-    /*()=>{
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-        
-    };*/
 
-  },[]);
-return status;
+const useFullScreen  = () =>{
+  const element = useRef();
+  const triggerFull = () =>{
+    if(element.current){
+      element.current.requestFullScreen();
+    }
+  }
+  const exitFull = () =>{
+    document.exitFullscreen();
+  }
+  return { element, triggerFull, exitFull};
 }
 const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online?"we just went online": "we are offline");
-  }
-  const onLine = useNetwork(handleNetworkChange);
+  const { element, triggerFull,exitFull } = useFullScreen();
   return (
-    <div>
-      <h1>{onLine?"online":"offline"}</h1>
+    <div >
+      <div ref={element}>
+      <img 
+        ref={element}
+        src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*"
+      />
+      <button onClick={triggerFull}>Make fullscreen</button>
+      <h1 >hello</h1>
+      </div>
+      <button onClick={exitFull}>Exit fullscreen</button>
     </div>
   );
 }
