585c13552bba02dc4816eb86b0998a129b9ee5b8
onethegarden <onethegarden@gmail.com>
2020-03-25 08:00:18 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -1,59 +1,35 @@
 import React, {useState, useRef, useEffect} from 'react';
 import './App.css';
 
-const useFullscreen = callback => {
-  const element = useRef();
-  const runCb = isFull => {
-    if (callback && typeof callback === "function") {
-      callback(isFull);
-    }
-  };
-  const triggerFull = () => {
-    if (element.current) {
-      if (element.current.requestFullscreen) {
-        element.current.requestFullscreen();
-      } else if (element.current.mozRequestFullScreen) {
-        element.current.mozRequestFullScreen();
-      } else if (element.current.webkitRequestFullscreen) {
-        element.current.webkitRequestFullscreen();
-      } else if (element.current.msRequestFullscreen) {
-        element.current.msRequestFullscreen();
-      }
-      runCb(true);
-    }
-  };
-  const exitFull = () => {
-    document.exitFullscreen();
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-    runCb(false);
-  };
-  return { element, triggerFull, exitFull };
-};
 
-const App = () => {
-  const onfulls = isFull => {
-    console.log(isFull?"we are full":"we still small");
+const useNotification =(title, options)=>{
+  console.log("?")
+  if(!("Notification" in window)){
+    console.log("return")
+    return;
   }
-  const { element, triggerFull,exitFull } = useFullscreen();
+  const fireNotif = ()=>{
+    if(Notification.permission!=="granted"){
+      Notification.requestPermission().then(permission=>{
+        console.log("if")
+        if(permission ==="granted"){
+          new Notification(title, options);
+        }else{
+          return;
+        }
+      })
+    }else{
+      console.log("else")
+      new Notification(title, options);
+    }
+    return fireNotif;
+  }
+}
+const App = () => {
+  const triggerNotif = useNotification("can i steal your kimchi", {body:"i love kimchi dont you?"});
   return (
     <div >
-      <div ref={element}>
-      <img 
-      width="200px"
-        ref={element}
-        src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*"
-      />
-      <button onClick={triggerFull}>Make fullscreen</button>
-      </div>
-      <button onClick={exitFull}>Exit fullscreen</button>
+      <button onClick={triggerNotif}>hello</button>
     </div>
   );
 }
